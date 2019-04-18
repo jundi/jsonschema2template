7fc5b82023a213a699f86e43fe2a86d62369f902
Heikki Mikkolainen <heikki.mikkolainen@csc.fi>
2019-04-18 15:11:35 +0300

    Add install requirements

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ import setuptools
 setuptools.setup(
     name='jsonschema2template',
     py_modules=['jsonschema2template'],
+    install_requires=['argcomplete'],
     entry_points={
         'console_scripts': [
             'jsonschema2template = jsonschema2template:main'
