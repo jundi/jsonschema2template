ca46b66e718dfb6a34ca96cabccf24f64feed53a
Heikki Mikkolainen <heikki.mikkolainen@csc.fi>
2019-04-23 14:02:42 +0300

    Require python3

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ import setuptools
 setuptools.setup(
     name='jsonschema2template',
     py_modules=['jsonschema2template'],
+    python_requires='>=3',
     install_requires=['argcomplete'],
     entry_points={
         'console_scripts': [
